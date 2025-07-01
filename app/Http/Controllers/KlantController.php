a09cd0c1b2448b2b3252b17b782850753e134178
Artycurus <339710@student.mboutrecht.nl>
2025-07-01 13:56:53 +0200

    data is er

--- app/Http/Controllers/KlantController.php
+++ app/Http/Controllers/KlantController.php
@@ -21,50 +21,144 @@ class KlantController extends Controller
             })
             ->get();
 
-        return view('klanten.index', compact('gezinnen', 'postcodes'));
+        // Dummy testdata als er geen echte gezinnen zijn
+        if ($gezinnen->isEmpty()) {
+            $gezinnen = collect([
+                [
+                    'id' => 1,
+                    'voornaam' => 'Jan',
+                    'tussenvoegsel' => 'van',
+                    'achternaam' => 'Jansen',
+                    'geboortedatum' => '1980-01-01',
+                    'type_persoon' => 'Hoofdaanvrager',
+                    'vertegenwoordiger' => 'Ja',
+                    'straatnaam' => 'Dorpsstraat',
+                    'huisnummer' => '1',
+                    'toevoeging' => 'A',
+                    'postcode' => '1234AB',
+                    'woonplaats' => 'Maaskantje',
+                    'email' => 'jan.jansen@email.com',
+                    'mobiel' => '06-12345678',
+                ],
+                [
+                    'id' => 2,
+                    'voornaam' => 'Anja',
+                    'tussenvoegsel' => '',
+                    'achternaam' => 'Bakker',
+                    'geboortedatum' => '1975-05-10',
+                    'type_persoon' => 'Partner',
+                    'vertegenwoordiger' => 'Nee',
+                    'straatnaam' => 'Molenstraat',
+                    'huisnummer' => '5',
+                    'toevoeging' => '',
+                    'postcode' => '1111AA',
+                    'woonplaats' => 'Maaskantje',
+                    'email' => 'anja.bakker@email.com',
+                    'mobiel' => '06-23456789',
+                ],
+                // Voeg meer testgezinnen toe indien gewenst
+            ]);
+        }
+
+        return view('klanten.index', [
+            'gezinnen' => $gezinnen,
+            'postcodes' => $postcodes,
+        ]);
     }
 
     public function show($id)
     {
-        // Haal gewijzigde klant uit sessie als die er is, anders dummy data
-        $klant = session('klant_'.$id) ?? [
-            'id' => $id,
-            'voornaam' => 'Jan',
-            'tussenvoegsel' => '',
-            'achternaam' => 'Jansen',
-            'geboortedatum' => '1980-01-01',
-            'type_persoon' => 'Hoofdaanvrager',
-            'vertegenwoordiger' => 'Ja',
-            'straatnaam' => 'Dorpsstraat',
-            'huisnummer' => '1',
-            'toevoeging' => '',
-            'postcode' => '1234AB',
-            'woonplaats' => 'Maaskantje',
-            'email' => 'jan.jansen@email.com',
-            'mobiel' => '06-12345678',
-        ];
+        // Zoek klant in sessie (na wijziging)
+        $klant = session('klant_'.$id);
+
+        // Zoek klant in testdata als niet in sessie
+        if (!$klant) {
+            $testData = [
+                1 => [
+                    'id' => 1,
+                    'voornaam' => 'Jan',
+                    'tussenvoegsel' => 'van',
+                    'achternaam' => 'Jansen',
+                    'geboortedatum' => '1980-01-01',
+                    'type_persoon' => 'Hoofdaanvrager',
+                    'vertegenwoordiger' => 'Ja',
+                    'straatnaam' => 'Dorpsstraat',
+                    'huisnummer' => '1',
+                    'toevoeging' => 'A',
+                    'postcode' => '1234AB',
+                    'woonplaats' => 'Maaskantje',
+                    'email' => 'jan.jansen@email.com',
+                    'mobiel' => '06-12345678',
+                ],
+                2 => [
+                    'id' => 2,
+                    'voornaam' => 'Anja',
+                    'tussenvoegsel' => '',
+                    'achternaam' => 'Bakker',
+                    'geboortedatum' => '1975-05-10',
+                    'type_persoon' => 'Partner',
+                    'vertegenwoordiger' => 'Nee',
+                    'straatnaam' => 'Molenstraat',
+                    'huisnummer' => '5',
+                    'toevoeging' => '',
+                    'postcode' => '1111AA',
+                    'woonplaats' => 'Maaskantje',
+                    'email' => 'anja.bakker@email.com',
+                    'mobiel' => '06-23456789',
+                ],
+                // Voeg meer testgezinnen toe indien gewenst
+            ];
+            $klant = $testData[$id] ?? $testData[1];
+        }
+
         return view('klanten.show', compact('klant'));
     }
 
     public function edit($id)
     {
-        // Dummy klant voor test
-        $klant = [
-            'id' => $id,
-            'voornaam' => 'Jan',
-            'tussenvoegsel' => '',
-            'achternaam' => 'Jansen',
-            'geboortedatum' => '1980-01-01',
-            'type_persoon' => 'Hoofdaanvrager',
-            'vertegenwoordiger' => 'Ja',
-            'straatnaam' => 'Dorpsstraat',
-            'huisnummer' => '1',
-            'toevoeging' => '',
-            'postcode' => '1234AB',
-            'woonplaats' => 'Maaskantje',
-            'email' => 'jan.jansen@email.com',
-            'mobiel' => '06-12345678',
-        ];
+        // Zoek klant in sessie (na wijziging)
+        $klant = session('klant_'.$id);
+
+        // Zoek klant in testdata als niet in sessie
+        if (!$klant) {
+            $testData = [
+                1 => [
+                    'id' => 1,
+                    'voornaam' => 'Jan',
+                    'tussenvoegsel' => 'van',
+                    'achternaam' => 'Jansen',
+                    'geboortedatum' => '1980-01-01',
+                    'type_persoon' => 'Hoofdaanvrager',
+                    'vertegenwoordiger' => 'Ja',
+                    'straatnaam' => 'Dorpsstraat',
+                    'huisnummer' => '1',
+                    'toevoeging' => 'A',
+                    'postcode' => '1234AB',
+                    'woonplaats' => 'Maaskantje',
+                    'email' => 'jan.jansen@email.com',
+                    'mobiel' => '06-12345678',
+                ],
+                2 => [
+                    'id' => 2,
+                    'voornaam' => 'Anja',
+                    'tussenvoegsel' => '',
+                    'achternaam' => 'Bakker',
+                    'geboortedatum' => '1975-05-10',
+                    'type_persoon' => 'Partner',
+                    'vertegenwoordiger' => 'Nee',
+                    'straatnaam' => 'Molenstraat',
+                    'huisnummer' => '5',
+                    'toevoeging' => '',
+                    'postcode' => '1111AA',
+                    'woonplaats' => 'Maaskantje',
+                    'email' => 'anja.bakker@email.com',
+                    'mobiel' => '06-23456789',
+                ],
+                // Voeg meer testgezinnen toe indien gewenst
+            ];
+            $klant = $testData[$id] ?? $testData[1];
+        }
+
         // Als er oude input is (na submit), vul die in
         foreach ($klant as $key => $value) {
             if (old($key) !== null) {
@@ -79,12 +173,21 @@ class KlantController extends Controller
         // Simuleer validatie
         $data = $request->validate([
             'voornaam' => 'required',
+            'tussenvoegsel' => 'nullable',
             'achternaam' => 'required',
+            'geboortedatum' => 'nullable',
+            'type_persoon' => 'nullable',
+            'vertegenwoordiger' => 'nullable',
+            'straatnaam' => 'nullable',
+            'huisnummer' => 'nullable',
+            'toevoeging' => 'nullable',
+            'postcode' => 'nullable',
+            'woonplaats' => 'nullable',
             'email' => 'required|email',
-            // ...andere velden indien gewenst...
+            'mobiel' => 'nullable',
         ]);
 
-        // Simuleer update: sla gewijzigde data tijdelijk op in de sessie
+        // Simuleer update: sla ALLE gewijzigde data tijdelijk op in de sessie
         session(['klant_'.$id => $data + ['id' => $id]]);
 
         // Redirect met melding en na 3 seconden terug naar details
--- resources/views/klanten/show.blade.php
+++ resources/views/klanten/show.blade.php
@@ -13,27 +13,26 @@
         @endif
         <table class="w-full mb-8 border border-gray-200 rounded">
             <tbody>
-                <tr><td class="py-2 px-4 font-semibold w-1/3">Voornaam</td><td class="py-2 px-4">{{ $klant['voornaam'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Tussenvoegsel</td><td class="py-2 px-4">{{ $klant['tussenvoegsel'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Achternaam</td><td class="py-2 px-4">{{ $klant['achternaam'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Geboortedatum</td><td class="py-2 px-4">{{ $klant['geboortedatum'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">TypePersoon</td><td class="py-2 px-4">{{ $klant['type_persoon'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Vertegenwoordiger</td><td class="py-2 px-4">{{ $klant['vertegenwoordiger'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Straatnaam</td><td class="py-2 px-4">{{ $klant['straatnaam'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Huisnummer</td><td class="py-2 px-4">{{ $klant['huisnummer'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Toevoeging</td><td class="py-2 px-4">{{ $klant['toevoeging'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Postcode</td><td class="py-2 px-4">{{ $klant['postcode'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Woonplaats</td><td class="py-2 px-4">{{ $klant['woonplaats'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Email</td><td class="py-2 px-4">{{ $klant['email'] ?? '~~~~' }}</td></tr>
-                <tr><td class="py-2 px-4 font-semibold">Mobiel</td><td class="py-2 px-4">{{ $klant['mobiel'] ?? '~~~~' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold w-1/3">Voornaam</td><td class="py-2 px-4">{{ $klant['voornaam'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Tussenvoegsel</td><td class="py-2 px-4">{{ $klant['tussenvoegsel'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Achternaam</td><td class="py-2 px-4">{{ $klant['achternaam'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Geboortedatum</td><td class="py-2 px-4">{{ $klant['geboortedatum'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">TypePersoon</td><td class="py-2 px-4">{{ $klant['type_persoon'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Vertegenwoordiger</td><td class="py-2 px-4">{{ $klant['vertegenwoordiger'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Straatnaam</td><td class="py-2 px-4">{{ $klant['straatnaam'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Huisnummer</td><td class="py-2 px-4">{{ $klant['huisnummer'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Toevoeging</td><td class="py-2 px-4">{{ $klant['toevoeging'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Postcode</td><td class="py-2 px-4">{{ $klant['postcode'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Woonplaats</td><td class="py-2 px-4">{{ $klant['woonplaats'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Email</td><td class="py-2 px-4">{{ $klant['email'] ?? '' }}</td></tr>
+                <tr><td class="py-2 px-4 font-semibold">Mobiel</td><td class="py-2 px-4">{{ $klant['mobiel'] ?? '' }}</td></tr>
             </tbody>
         </table>
         <div class="flex flex-wrap gap-2 justify-end">
-            <a href="{{ route('klanten.edit', $klant['id'] ?? 1) }}" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded font-semibold text-base shadow transition">Wijzig</a>
+            <a href="{{ route('klanten.edit', $klant['id']) }}" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded font-semibold text-base shadow transition">Wijzig</a>
             <a href="{{ route('klanten.index') }}" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded font-semibold text-base shadow transition">Terug</a>
             <a href="{{ route('dashboard') }}" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded font-semibold text-base shadow transition">Home</a>
         </div>
     </div>
 </x-app-layout>
-      
-  
+
